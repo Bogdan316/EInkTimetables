7665222c9d59901c2d7edb9c96b5938ca3bf4400
BOGDAN <bogdan.pirtica@student.upt.ro>
2023-04-15 14:55:50 +0300

    create branch for client

--- flutter-frontend/eink_timetables/lib/exceptions.dart
+++
@@ -1,9 +0,0 @@
-class UnfulfilledRequestException implements Exception {
-  String msg;
-  UnfulfilledRequestException(this.msg);
-}
-
-class RaspPiAlreadyRegisteredException implements Exception {
-  String msg;
-  RaspPiAlreadyRegisteredException(this.msg);
-}
