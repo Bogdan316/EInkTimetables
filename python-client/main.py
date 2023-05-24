4d9d3f8e2ea0479eb080a4db5c42969933841638
BOGDAN <bogdan.pirtica@student.upt.ro>
2023-05-24 23:00:16 +0300

    update script

--- python-client/main.py
+++ python-client/main.py
@@ -3,6 +3,7 @@ import time
 
 from PIL import Image
 from fastapi import FastAPI, UploadFile
+from inky import InkyWHAT
 from inky.auto import auto
 
 app = FastAPI()
@@ -11,8 +12,10 @@ app = FastAPI()
 @app.post('/upload-timetable/')
 async def upload_image(file: UploadFile):
     contents = await file.read()
-
-    screen = auto()
+    try:
+        screen = auto()
+    except RuntimeError:
+        screen = InkyWHAT('black')
 
     img = Image.open(io.BytesIO(contents))
 
@@ -22,22 +25,10 @@ async def upload_image(file: UploadFile):
 
     h_new = 300
     w_new = int((float(w) / h) * h_new)
-    w_cropped = 400
 
     # Resize the image with high-quality resampling
 
     img = img.resize((w_new, h_new), resample=Image.LANCZOS)
-
-    # Calculate coordinates to crop image to 400 pixels wide
-
-    x0 = (w_new - w_cropped) // 2
-    x1 = x0 + w_cropped
-    y0 = 0
-    y1 = h_new
-
-    # Crop image
-
-    img = img.crop((x0, y0, x1, y1))
 
     # Convert the image to use a white / black / red colour palette
 
@@ -56,7 +47,10 @@ async def upload_image(file: UploadFile):
 
 @app.post('/clear-screen/')
 async def clear_screen(cycles: int = 1):
-    screen = auto()
+    try:
+        screen = auto()
+    except RuntimeError:
+        screen = InkyWHAT('black')
 
     colours = (screen.BLACK, screen.WHITE)
 
@@ -65,7 +59,7 @@ async def clear_screen(cycles: int = 1):
 
     # Loop through the specified number of cycles and completely
     # fill the display with each colour in turn.
-    for i in range(cycles):
+    for _ in range(cycles):
         for color in colours:
             img.paste(color, (0, 0, img.size[0], img.size[1]))
             screen.set_image(img)
